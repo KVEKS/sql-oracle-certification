abd85454f5bcb0218dd8415e6d2b36840d626fa7
Aleksandr Avilov <52620005+KVEKS@users.noreply.github.com>
2020-05-20 22:59:24 +0300

    Update 02_Введение в sql.sql

--- 02_Введение в sql.sql
+++ 02_Введение в sql.sql
@@ -8,7 +8,7 @@ select first_name|| '(' || job_id || ')' as "our_employees" from employees;
 
 select distinct first_name from employees;
 
-select JOB_TITLE, 'min='||min_salary||', max='||max_salary as "info", 
+select job_title, 'min='||min_salary||', max='||max_salary as "info", 
 max_salary as "max", max_salary*2-2000 as "new_salary" from jobs;
 
 select q'<Peters's dog is very clever>' as result from dual;
